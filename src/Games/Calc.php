25a8549e7293d4c889ef1e9fc5140702267ee53d
Vladislav Panferov <vladislav.panferov@gmail.com>
2021-01-15 23:00:03 +0700

    add brain-calc game

---
+++ src/Games/Calc.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Brain\Games\Games\Calc;
+
+use function Brain\Games\Engine\play;
+
+const MIN = 1;
+const MAX = 11;
+const OPERATORS = ['+', '-', '*'];
+const INSTRUCTIONS = "What is the result of the expression?";
+
+function getQuestion(): array
+{
+    $a = rand(MIN, MAX);
+    $b = rand(MIN, MAX);
+    $operator = OPERATORS[array_rand(OPERATORS)];
+    return [$a, $operator, $b];
+}
+
+function getAnswer(array $question): int
+{
+    [$a, $operator, $b] = $question;
+    switch ($operator) {
+        case '+':
+            return $a + $b;
+        case '-':
+            return $a - $b;
+        case '*':
+            return $a * $b;
+    }
+}
+
+function start(): void
+{
+    $getRound = function (): array {
+        $question = getQuestion();
+        $questionSting = implode(' ', $question);
+        $answer = (string) getAnswer($question);
+        return [$questionSting, $answer];
+    };
+    play(INSTRUCTIONS, $getRound);
+}
