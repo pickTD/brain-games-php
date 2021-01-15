5f6add65786e4ffe8f589f1fe120c1117010040b
Vladislav Panferov <vladislav.panferov@gmail.com>
2021-01-15 23:00:44 +0700

    add brain-prime game

---
+++ src/Games/Prime.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Brain\Games\Games\Prime;
+
+use function Brain\Games\Engine\play;
+
+const MIN = 1;
+const MAX = 101;
+const INSTRUCTIONS = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+
+function isPrime(int $number): bool
+{
+    if ($number < 2) {
+        return false;
+    } elseif ($number < 4) {
+        return true;
+    }
+    for ($i = 2, $max = sqrt($number); $i < $max; $i++) {
+        if (!($number % $i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function start(): void
+{
+    $getRound = function (): array {
+        $question = rand(MIN, MAX);
+        $answer = isPrime($question) ? "yes" : "no";
+        return [$question, $answer];
+    };
+    play(INSTRUCTIONS, $getRound);
+}
