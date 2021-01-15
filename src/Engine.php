817a5cff6d804d72a3d08d55d3fcf4ca505ae2a2
Vladislav Panferov <vladislav.panferov@gmail.com>
2021-01-15 22:59:24 +0700

    add games engine

---
+++ src/Engine.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Brain\Games\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+const GOAL = 3;
+
+function play(string $instructions, callable $getRound): void
+{
+    line("Welcome to the Brain Games!");
+    $name = prompt("May I have your name? ", false, '');
+    line("Hello, %s!", $name);
+    line($instructions);
+    $answersCount = 0;
+
+    while ($answersCount < GOAL) {
+        [$question, $correctAnswer] = $getRound();
+        line("Question: %s", $question);
+        $answer = prompt("Your answer: ", false, '');
+        if ($answer !== $correctAnswer) {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
+            line("Let's try again, %s!", $name);
+            break;
+        }
+        line("Correct!");
+        $answersCount += 1;
+    }
+    if ($answersCount === GOAL) {
+        line("Congratulations, %s!", $name);
+    }
+}
