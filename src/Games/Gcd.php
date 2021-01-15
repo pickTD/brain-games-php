45cf09067169120adad8e0074681012d41047e3d
Vladislav Panferov <vladislav.panferov@gmail.com>
2021-01-15 23:00:13 +0700

    add brain-gcd game

---
+++ src/Games/Gcd.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Brain\Games\Games\Gcd;
+
+use function Brain\Games\Engine\play;
+
+const MIN = 1;
+const MAX = 9;
+const INSTRUCTIONS = "Find the greatest common divisor of given numbers.";
+
+function getQuestion(): array
+{
+    $base = rand(MIN, MAX);
+    $mul1 = rand(MIN, MAX);
+    $mul2 = rand(MIN, MAX);
+    $a = $base * $mul1;
+    $b = $base * $mul2;
+    return [$a, $b];
+}
+
+function gcd($a, $b)
+{
+    return ($a % $b) ? gcd($b, $a % $b) : $b;
+}
+
+function start(): void
+{
+    $getRound = function (): array {
+        $question = getQuestion();
+        $questionSting = implode(' ', $question);
+        $answer = (string) gcd(...$question);
+        return [$questionSting, $answer];
+    };
+    play(INSTRUCTIONS, $getRound);
+}
